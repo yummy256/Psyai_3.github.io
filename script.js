e7f2557dd452c1ddb133f2514385cf58da272ab5
yummy256 <3217638201@qq.com>
2025-12-13 13:29:35 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ let emotionData = [];
 let currentEmotion = '中性';
 let lastMessageEmotion = '中性';
 let historySessions = JSON.parse(localStorage.getItem('historySessions') || '[]');
-let rasaApiUrl = localStorage.getItem('rasaApiUrl') || 'http://localhost:5005/webhooks/rest/webhook';
+let rasaApiUrl = localStorage.getItem('rasaApiUrl') || 'https://brown-mobiles-exposed-currently.trycloudflare.com/webhooks/rest/webhook';
 let isLoading = false;
 let isPanelOpen = true;
 
